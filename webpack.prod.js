54431eea8a7da6c3258dc94f5d269edb691d51ce
Elena Lambova <elena.g.lambova@gmail.com>
2020-04-19 22:53:57 +0300

    Refactor: Add service-worker in index.html and webpack.prod.js.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-// const WorkboxPlugin = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -44,6 +44,6 @@ module.exports = {
       favicon: "./src/client/images/favicon.ico",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
-    // new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
